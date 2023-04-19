d1596d8bae06d4dd0e4ae0b69ea4258127176300
Matheus Duplat <matheusduplat@hotmail.com>
2023-04-19 19:47:56 -0300

    modulo final 1/2 concluido

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,77 @@
+import {stripe} from "@/lib/stripe";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/styles/pages/product";
+import {GetStaticPaths, GetStaticProps} from "next";
+import Image from "next/image";
 import {useRouter} from "next/router";
+import Stripe from "stripe";
 
-export default function Product() {
-  const {query} = useRouter();
-
-  return <h1>produtos: {query.id}</h1>;
+interface ProductPros {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  };
 }
+
+export default function Product({product}: ProductPros) {
+  const {isFallback} = useRouter();
+
+  if (isFallback) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+
+        <p>{product.description}</p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{params: {id: "prod_NibyrVqWfu5YQw"}}],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<any, {id: string}> = async ({
+  params,
+}) => {
+  const productId = String(params?.id);
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ["default_price"],
+  });
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat("pt-br", {
+          style: "currency",
+          currency: "BRL",
+        }).format((price.unit_amount as number) / 100),
+        description: product.description,
+      },
+    },
+    revalidate: 60 * 60 * 1,
+  };
+};
